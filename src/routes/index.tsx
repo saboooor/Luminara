9ec4188d91802e79ca545e5e5c703ec6d05a99a6
saboooor <saboor547@gmail.com>
2023-01-15 17:48:16 -0700

    fuck you typescript

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 import Hero from '~/components/sections/Hero';
 import Why from '~/components/sections/Why';
-import Servers from '~/components/sections/Servers';
 import Features from '~/components/sections/Features';
 
 export default component$(() => {
