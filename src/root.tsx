1120c798825bfa6a2701247fbc62b75ec8d9cb8e
saboooor <saboor547@gmail.com>
2023-01-15 17:32:42 -0700

    apply bg blur on already-scrolled

--- src/root.tsx
+++ src/root.tsx
@@ -20,6 +20,27 @@ export const onScroll$ = (fnQrl: QRL<()=>void>) => {
   });
 }
 
+export const blur = () => {
+  // Get the backdrop element
+  const backDrop = document.getElementById('backdrop');
+
+  // Check the scroll distance and round it up
+  const scrollTop = Math.ceil(document.getElementsByTagName('html')[0].scrollTop);
+
+  // Set the max blur value (300 / 6 = 50)
+  let blurVal = 50;
+
+  // Set the blur value to (scrollTop / 6) if scroll distance is under 300
+  if (scrollTop < 300) blurVal = Math.round(scrollTop / 6);
+
+  // Set blur value with padding for the zoom as well
+  blurVal = String(blurVal).padStart(2, '0');
+  
+  // Set filter with blur valur and scale as a decimal: 1.{blur value}
+  backDrop.style.filter = `blur(${blurVal}px)`;
+  backDrop.style.transform = `scale(1.${blurVal})`;
+};
+
 export default component$(() => {
   /**
    * The root of a QwikCity site always start with the <QwikCity> component,
@@ -30,26 +51,8 @@ export default component$(() => {
   useStyles$(globalStyles);
 
   useClientEffect$(()=>{
-    onScroll$(() => {
-      // Get the backdrop element
-      const backDrop = document.getElementById('backdrop');
-
-      // Check the scroll distance and round it up
-      const scrollTop = Math.ceil(document.getElementsByTagName('html')[0].scrollTop);
-    
-      // Set the max blur value (300 / 6 = 50)
-      let blurVal = 50;
-    
-      // Set the blur value to (scrollTop / 6) if scroll distance is under 300
-      if (scrollTop < 300) blurVal = Math.round(scrollTop / 6);
-    
-      // Set blur value with padding for the zoom as well
-      blurVal = String(blurVal).padStart(2, '0');
-      
-      // Set filter with blur valur and scale as a decimal: 1.{blur value}
-      backDrop.style.filter = `blur(${blurVal}px)`;
-      backDrop.style.transform = `scale(1.${blurVal})`;
-    });
+    blur()
+    onScroll$(() => blur());
   })
 
   return (
