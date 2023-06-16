ba5aebaa80340d7ba99ff3662f1313e488f88bb7
saboooor <saboor547@gmail.com>
2023-06-16 02:03:14 -0600

    new votesites, switch out pmc for tmcs

--- src/routes/vote/index.tsx
+++ src/routes/vote/index.tsx
@@ -10,60 +10,55 @@ export default component$(() => {
 
   return (
     <section class="flex mx-auto px-6 items-center justify-center text-center" style="min-height: calc(100lvh - 64px);">
-      <div class="p-10 sm:p-16 drop-shadow-xl rounded-xl text-orange-100 max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl">
+      <div class="p-10 sm:p-16 drop-shadow-xl rounded-xl text-orange-100 max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl">
         <h1 class="font-bold text-3xl sm:text-5xl mb-3 shadow-outline">
           Vote for <span class="text-red-500">Nether Depths!</span>
         </h1>
         <p class="mb-6 shadow-outline">
           Hold CTRL while clicking to open each site in multiple tabs
         </p>
-        <div class="mt-6 mb-12 grid grid-cols-3 md:grid-cols-6 gap-3 justify-center">
-          <a href="https://bit.ly/NetherMCSL" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+        <p class="mb-6 shadow-outline">
+          The blue buttons do not give any reward and are purely a way to support Nether Depths :)
+        </p>
+        <div class="mt-6 mb-12 flex flex-wrap gap-3 justify-center">
+          <a href="https://bit.ly/NetherVote-MCSL" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
             MCSL
           </a>
-          <a href="https://bit.ly/NetherMCSO" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+          <a href="https://bit.ly/NetherVote-MCSO" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
             MCSO
           </a>
-          <a href="https://bit.ly/NetherMSCN" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+          <a href="https://bit.ly/NetherVote-MCSN" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
             MCSN
           </a>
-          <a href="https://bit.ly/NetherMCMP" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+          <a href="https://bit.ly/NetherVote-MCMP" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
             MCMP
           </a>
-          <a href="https://bit.ly/NetherMCSP" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+          <a href="https://bit.ly/NetherVote-MCPS" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
             MCPS
           </a>
-          <a href="https://bit.ly/NetherPMC"  class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
-            PMC
-          </a>
-        </div>
-        <h1 class="font-bold text-3xl sm:text-5xl mb-3 shadow-outline">
-          Optional Vote Sites
-        </h1>
-        <p class="mb-6 shadow-outline">
-          Please support Nether Depths through the sites below! These sites don't give any reward though.
-        </p>
-
-        <div class="mt-6 flex flex-wrap gap-3 justify-center">
-          <a href="https://bit.ly/NetherTOPG" class="flex transition rounded-xl bg-indigo-900/80 hover:bg-indigo-900 border-indigo-900 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
-            TOPG.ORG
-          </a>
-          <a href="https://bit.ly/NetherTMCS" class="flex transition rounded-xl bg-indigo-900/80 hover:bg-indigo-900 border-indigo-900 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+          <a href="https://bit.ly/NetherVote-TMCS" class="flex transition rounded-xl bg-red-600/80 hover:bg-red-600 border-red-600 border px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
             TMCS
           </a>
-          <a href="https://bit.ly/NetherMCSH" class="flex transition rounded-xl bg-indigo-900/80 hover:bg-indigo-900 border-indigo-900 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+          <a href="https://bit.ly/NetherVote-CF" class="flex transition rounded-xl bg-cyan-700/70 hover:bg-cyan-700 border-cyan-700 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+            CurseForge
+          </a>
+          <a href="https://bit.ly/NetherVote-SMCN" class="flex transition rounded-xl bg-cyan-700/70 hover:bg-cyan-700 border-cyan-700 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+            SMCN
+          </a>
+          <a href="https://bit.ly/NetherVote-MCSH" class="flex transition rounded-xl bg-cyan-700/70 hover:bg-cyan-700 border-cyan-700 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
             MCSH
           </a>
-          <a href="https://disboard.org/server/865519986806095902" class="flex transition rounded-xl bg-indigo-900/80 hover:bg-indigo-900 border-indigo-900 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
-            DISBOARD
+          <a href="https://bit.ly/NetherVote-PMC"  class="flex transition rounded-xl bg-cyan-700/70 hover:bg-cyan-700 border-cyan-700 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+            PMC
           </a>
-          <a href="https://top.gg/servers/865519986806095902/vote" class="flex transition rounded-xl bg-indigo-900/80 hover:bg-indigo-900 border-indigo-900 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
-            TOP.GG
+          <a href="https://bit.ly/NetherVote-TOPG" class="flex transition rounded-xl bg-cyan-700/70 hover:bg-cyan-700 border-cyan-700 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+            TopG
           </a>
-          <a href="https://top.gg/bot/848775888673439745/vote" class="flex transition rounded-xl bg-indigo-900/80 hover:bg-indigo-900 border-indigo-900 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
-            TOP.GG (Cactie)
+          <a href="https://top.gg/servers/865519986806095902/vote" class="flex transition rounded-xl bg-cyan-700/70 hover:bg-cyan-700 border-cyan-700 border px-6 py-3 font-bold text-indigo-100 md:py-4 md:px-8 md:text-lg whitespace-nowrap justify-center">
+            Top.gg
           </a>
         </div>
+
       </div>
     </section>
   );
