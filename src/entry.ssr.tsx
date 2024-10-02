a7b3f7d2c5300a7fc564ff9b1166111ecfa064a4
saboooor <saboor547@gmail.com>
2024-10-01 20:58:38 -0600

    fix?

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -18,8 +18,10 @@ export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
+    // Use container attributes to set attributes on the html tag.
     containerAttributes: {
-      lang: 'en',
+      lang: 'en-us',
+      ...opts.containerAttributes,
     },
     prefetchStrategy: {
       implementation: {
--- src/root.tsx
+++ src/root.tsx
@@ -1,33 +1,10 @@
-import { component$, useStyles$, QRL } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/head';
 
-import globalStyles from './global.css?inline';
-
-export function onScroll(fnQrl: QRL<() => void>) {
-  document.addEventListener('scroll', async () => {
-    const fn = await fnQrl.resolve();
-    fn();
-  });
-}
-export const onScrollQrl = onScroll;
-
-export const onScroll$: any = (fnQrl: QRL<()=>void>) => {
-  document.addEventListener('scroll', async () => {
-    const fn = await fnQrl.resolve();
-    fn();
-  });
-};
+import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCity> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
-  useStyles$(globalStyles);
-
   return (
     <QwikCityProvider>
       <head>
