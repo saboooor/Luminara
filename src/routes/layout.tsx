f03d52bc25eec157b72a482c2d92365d2b4c922a
saboooor <saboor547@gmail.com>
2024-10-01 21:58:58 -0600

    cherry leaves

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -23,37 +23,34 @@ export default component$(() => {
       y: number;
       radius: number;
       speed: number;
-      directionX: number;
       color: string;
+      img: HTMLImageElement;
 
       constructor() {
         this.x = Math.random() * canvas.width; // Random x position
         this.y = Math.random() * canvas.height; // Random y position
-        this.radius = Math.random() * 1 + 3; // Random size
+        this.radius = Math.random() * 10 + 10; // Random size
         this.speed = Math.random() * 0.5 + 1; // Random speed
-        this.directionX = Math.random() * 2 - 1; // Random x direction
         this.color = 'white'; // White color
+        this.img = new Image();
+        this.img.src = `/particle/cherry_${Math.floor(Math.random() * 11)}.png`;
       }
       // Update particle position
       update() {
-        this.y -= this.speed * (loc.url.pathname != '/' ? 0.5 : 1);
-        this.x += this.directionX * this.speed * 0.4;
-        if (this.x > canvas.width || this.x < 0) {
-          this.directionX = -this.directionX; // Reverse direction if particle hits the wall
+        this.y += this.speed * (loc.url.pathname != '/' ? 0.5 : 1);
+        this.x += this.speed * 0.4;
+        if (this.y > canvas.height) {
+          this.y = 0; // Reset position if particle goes out of the canvas
         }
-        if (this.y < 0) {
-          this.y = canvas.height; // Reset position if particle goes out of the canvas
+        if (this.x > canvas.width) {
+          this.x = 0; // Reset position if particle goes out of the canvas
         }
       }
       // Draw particle
       draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = this.color;
-        // blur slightly
-        ctx.shadowColor = this.color;
-        ctx.shadowBlur = 10;
-        ctx.fill();
+        // use image as particle
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(this.img, this.x, this.y, this.radius, this.radius);
       }
     }
 
@@ -83,7 +80,9 @@ export default component$(() => {
           'transition-all duration-1000 fixed top-0 overflow-hidden -z-10 w-full h-full object-cover opacity-40': true,
           'blur-xl scale-110': loc.url.pathname != '/',
         }} />
-        <canvas id="particles" class="fixed top-0 overflow-hidden -z-10 w-full h-full"></canvas>
+        <canvas id="particles" class="fixed top-0 overflow-hidden -z-10 w-full h-full" style={{
+          imageRendering: 'pixelated',
+        }}></canvas>
         <Header/>
         <section>
           <Slot />
