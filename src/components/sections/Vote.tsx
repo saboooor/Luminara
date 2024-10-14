08e12d1755eac9ba20593474dc7fa0f69819a226
saboooor <saboor547@gmail.com>
2024-10-14 16:30:02 -0600

    a

--- src/components/sections/Vote.tsx
+++ src/components/sections/Vote.tsx
@@ -18,43 +18,23 @@ export default component$(({ noScrollIndicator }: any) => {
         If you're a Bedrock player, dont forget to put a _ before your gamertag!
       </p>
       <div class="flex flex-wrap gap-3 justify-center">
-        <a href="https://bit.ly/NetherVote-MCSL" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
+        <a href="https://minecraft-server-list.com/server/507849/vote/" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
           MCSL
         </a>
-        <a href="https://bit.ly/NetherVote-MCSO" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
+        <a href="https://minecraftservers.org/vote/667461" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
           MCSO
         </a>
-        <a href="https://bit.ly/NetherVote-MCSN" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
+        <a href="https://minecraft-server.net/vote/ssaabboooorr/" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
           MCSN
         </a>
-        <a href="https://bit.ly/NetherVote-MCMP" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
+        <a href="https://minecraft-mp.com/server/337205/vote/" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
           MCMP
         </a>
-        <a href="https://bit.ly/NetherVote-MCPS" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
+        <a href="https://minecraftpocket-servers.com/server/129088/vote/" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
           MCPS
         </a>
-        <a href="https://bit.ly/NetherVote-SMCN" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
+        <a href="https://servers-minecraft.net/server-luminara.40623/vote" class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
           SMCN
-        </a>
-      </div>
-      <div class="mb-12 flex flex-wrap gap-3 justify-center">
-        <a href="https://bit.ly/NetherVote-CF" class="lum-btn lum-pad-xl rounded-xl lum-bg-indigo-600 hover:lum-bg-indigo-500">
-          CurseForge
-        </a>
-        <a href="https://bit.ly/NetherVote-TMCS" class="lum-btn lum-pad-xl rounded-xl lum-bg-indigo-600 hover:lum-bg-indigo-500">
-          TMCS
-        </a>
-        <a href="https://bit.ly/NetherVote-MCSH" class="lum-btn lum-pad-xl rounded-xl lum-bg-indigo-600 hover:lum-bg-indigo-500">
-          MCSH
-        </a>
-        <a href="https://bit.ly/NetherVote-PMC"  class="lum-btn lum-pad-xl rounded-xl lum-bg-indigo-600 hover:lum-bg-indigo-500">
-          PMC
-        </a>
-        <a href="https://bit.ly/NetherVote-TOPG" class="lum-btn lum-pad-xl rounded-xl lum-bg-indigo-600 hover:lum-bg-indigo-500">
-          TopG
-        </a>
-        <a href="https://top.gg/servers/865519986806095902/vote" class="lum-btn lum-pad-xl rounded-xl lum-bg-indigo-600 hover:lum-bg-indigo-500">
-          Top.gg
         </a>
       </div>
       <h1 class="shadow-outline text-4xl font-bold text-white">
@@ -67,19 +47,13 @@ export default component$(({ noScrollIndicator }: any) => {
       <Toggle label="Include blue sites" checked={store.blue} onChange$={() => store.blue = !store.blue} onColor="purple" offColor="gray"/>
       <div class="mb-12 flex flex-wrap gap-3 justify-center">
         <button onClick$={() => {
-          window.open('https://bit.ly/NetherVote-MCSL');
-          window.open('https://bit.ly/NetherVote-MCSO');
-          window.open('https://bit.ly/NetherVote-MCSN');
-          window.open('https://bit.ly/NetherVote-MCMP');
-          window.open('https://bit.ly/NetherVote-MCPS');
-          window.open('https://bit.ly/NetherVote-SMCN');
+          window.open('https://minecraft-server-list.com/server/507849/vote');
+          window.open('https://minecraftservers.org/vote/667461');
+          window.open('https://minecraft-server.net/vote/ssaabboooorr');
+          window.open('https://minecraft-mp.com/server/337205/vote');
+          window.open('https://minecraftpocket-servers.com/server/129088/vote');
+          window.open('https://servers-minecraft.net/server-luminara.40623/vote');
           if (!store.blue) return;
-          window.open('https://bit.ly/NetherVote-CF');
-          window.open('https://bit.ly/NetherVote-TMCS');
-          window.open('https://bit.ly/NetherVote-MCSH');
-          window.open('https://bit.ly/NetherVote-PMC');
-          window.open('https://bit.ly/NetherVote-TOPG');
-          window.open('https://top.gg/servers/865519986806095902/vote');
         }} class="lum-btn lum-pad-xl rounded-xl lum-bg-luminescent-600 hover:lum-bg-luminescent-500">
           Open vote sites
         </button>
