66972c93875da229d3044c78e59e44335e3f0f65
saboooor <saboor547@gmail.com>
2025-08-03 05:05:23 -0600

    include size for bg image

--- src/components/backgrounds.tsx
+++ src/components/backgrounds.tsx
@@ -1,7 +1,8 @@
-import Background1 from '~/images/backgrounds/background1.png?jsx';
-import Background2 from '~/images/backgrounds/background2.png?jsx';
-import Background3 from '~/images/backgrounds/background3.png?jsx';
-import Background4 from '~/images/backgrounds/background4.png?jsx';
+// @ts-nocheck
+import Background1 from '~/images/backgrounds/background1.png?jsx&w=1280;1920;2560';
+import Background2 from '~/images/backgrounds/background2.png?jsx&w=1280;1920;2560';
+import Background3 from '~/images/backgrounds/background3.png?jsx&w=1280;1920;2560';
+import Background4 from '~/images/backgrounds/background4.png?jsx&w=1280;1920;2560';
 
 export default [
   Background1,
